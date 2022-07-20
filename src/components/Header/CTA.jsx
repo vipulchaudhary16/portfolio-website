bf49785bf34a23598791f18f667daa952da8e22c
Vipul R Chaudhary <vipulr6111@gmail.com>
2022-07-20 07:59:52 +0530

    some modification

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CV from '../../assets/resume.pdf'
+import RTPCR from '../../assets/0921113773.pdf'
 import './header.css'
 
 function CTA() {
@@ -7,6 +8,7 @@ function CTA() {
     <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's talk</a>
+        <a href={RTPCR} download className='btn btn-primary'>Vipul RTPCR</a>
     </div>
   )
 }
